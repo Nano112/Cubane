cd52d40906681228af782f1465ba70f7b4db2600
Harrison <35530362+Nano112@users.noreply.github.com>
2025-05-12 14:06:01 +0200

    feat: update version to 1.0.4 and enhance resource pack loading in AssetLoader class

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,46 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
-export default defineConfig({
-    build: {
-        outDir: 'dist',
-        lib: {
-            entry: resolve('./src/index.ts'),
-            name: 'Cubane',
-            fileName: (format) => `cubane.${format}.js`,
-            formats: ['es', 'umd']
-        },
-        sourcemap: true,
-        rollupOptions: {
-            external: ['three', 'jszip'],
-            output: {
-                globals: {
-                    three: 'THREE',
-                    jszip: 'JSZip'
+export default defineConfig(({ command }) => {
+    const config = {
+        define: {
+            'process.env': {},
+            'global': 'globalThis',
+        }
+    };
+
+    if (command === 'serve') {
+        // Development configuration
+        return {
+            ...config,
+            root: './dev',  // Set root directory to dev for development server
+            server: {
+                open: true,   // Automatically open browser
+            }
+        };
+    } else {
+        // Build configuration (unchanged)
+        return {
+            ...config,
+            build: {
+                outDir: 'dist',
+                lib: {
+                    entry: resolve('./src/index.ts'),
+                    name: 'Cubane',
+                    fileName: (format) => `cubane.${format}.js`,
+                    formats: ['es', 'umd']
+                },
+                sourcemap: true,
+                rollupOptions: {
+                    external: ['three', 'jszip'],
+                    output: {
+                        globals: {
+                            three: 'THREE',
+                            jszip: 'JSZip'
+                        }
+                    }
                 }
             }
-        }
-    },
-    define: {
-        'process.env': {},
-        'global': 'globalThis',
+        };
     }
 });
